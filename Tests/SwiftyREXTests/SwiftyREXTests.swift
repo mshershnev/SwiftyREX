c67a772af63a93b7b55f9e036fb464fb4a7357f5
m1k3 <mshershnev.dev@gmail.com>
2024-04-02 13:08:28 +0100

    Initial Commit

---
+++ Tests/SwiftyREXTests/SwiftyREXTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyREX
+
+final class SwiftyREXTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyREX().text, "Hello, World!")
+    }
+}
